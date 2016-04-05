5a64081cfb956a11abc7aad220d97d65729a712b
Johnathon Kwisses <johnkwisses@gmail.com>
2016-04-04 22:56:22 -0600

    add more functionality; docstrings

--- db_functions.py
+++ db_functions.py
@@ -3,6 +3,12 @@ import sqlite3
 
 
 def create_db(cursor):
+    """Attempts to create the table 'people' in database file if not already created.
+
+    Args:
+        cursor (sqlite3.Cursor): Cursor in database file.
+
+    """
     try:
         sql = 'create table people (name, number, email, notes)'
         cursor.execute(sql)
@@ -11,6 +17,12 @@ def create_db(cursor):
 
 
 def menu():
+    """Main menu for Rolodex.
+
+    Returns:
+        str: Command to be used in main().
+
+    """
     welcome = "\n\nWelcome to your Rolodex!\n"
     divider = "-" * len(welcome)
     hint = "\nType 'help' for more info"
@@ -21,6 +33,12 @@ def menu():
 
 
 def add_person(cursor):
+    """Adds a person and their information to the database.
+
+    Args:
+        cursor (sqlite3.Cursor): Cursor in database file.
+
+    """
     name = input("Person's name: ")
     number = input("Person's number: ")
     email = input("Person's email: ")
@@ -33,6 +51,12 @@ def add_person(cursor):
 
 
 def delete_person(cursor):
+    """Deletes a person and their information from the database.
+
+    Args:
+        cursor (sqlite3.Cursor): Cursor in database file.
+
+    """
     person = input("Person to delete: ")
     sql = 'delete from people where name="{}"'
     sql = sql.format(person)
@@ -40,7 +64,63 @@ def delete_person(cursor):
     print("Deleted {} from rolodex".format(person))
 
 
+def update_person(cursor):
+    """Updates a person's information in the database.
+
+    Args:
+        cursor (sqlite3.Cursor): Cursor in database file.
+
+    """
+    person = input("Person to change: ")
+    info_type = input("Info to change [name, number, email, notes]: ")
+    info_new = input("New info: ")
+    sql = 'select * from people order by name'
+    results = cursor.execute(sql)
+    people = results.fetchall()
+    individual = ()
+
+    sql = 'update people set {0}="{1}" where {0}="{2}"'
+
+    for p in people:
+        if p[0] == person:
+            individual = p
+
+    if info_type.lower() == "name":
+        sql = sql.format(info_type, info_new, individual[0])
+    elif info_type.lower() == "number":
+        sql = sql.format(info_type, info_new, individual[1])
+    elif info_type.lower() == "email":
+        sql = sql.format(info_type, info_new, individual[2])
+    elif info_type.lower() == "notes":
+        sql = sql.format(info_type, info_new, individual[3])
+
+    cursor.execute(sql)
+    print("Updated info!")
+
+
+def list_person(cursor):
+    """Lists a person and their information.
+
+    Args:
+        cursor (sqlite3.Cursor): Cursor in database file.
+
+    """
+    person = input("Person to list: ")
+    sql = 'select * from people where name="{}"'
+    results = cursor.execute(sql.format(person))
+    person_out = results.fetchall()
+
+    for p in person_out:
+        print(p)
+
+
 def list_people(cursor):
+    """Lists all people and their respective information.
+
+    Args:
+        cursor (sqlite3.Cursor): Cursor in database file.
+
+    """
     sql = "select * from people order by name"
     results = cursor.execute(sql)
     people = results.fetchall()
@@ -54,3 +134,75 @@ def list_people(cursor):
 
         print_out = "{name}\t{number}\t{email}\t{notes}"
         print(print_out.format(name=name, number=number, email=email, notes=notes))
+
+
+def name_lookup(cursor):
+    """Searches the database for user's input; letters of name(s) to lookup.
+
+    Note: The function will also work with a completed name.
+
+    Args:
+        cursor (sqlite3.Cursor): Cursor in database file.
+
+    """
+    letter = input("First letter of name(s): ")
+    sql = 'select * from people where name like "{}%"'
+    results = cursor.execute(sql.format(letter.upper()))
+    people = results.fetchall()
+
+    for person in people:
+        print(person)
+
+
+def number_lookup(cursor):
+    """Searches the database for user's input; digits of number(s) to lookup.
+
+    Note: The function will also work with a completed number.
+
+    Args:
+        cursor (sqlite3.Cursor): Cursor in database file.
+
+    """
+    number = input("Number to lookup: ")
+    sql = 'select * from people where number like "{}%"'
+    results = cursor.execute(sql.format(number))
+    people = results.fetchall()
+
+    for person in people:
+        print(person)
+
+
+def email_lookup(cursor):
+    """Searches the database for user's input; characters of email(s) to lookup.
+
+    Note: The function will also work with a completed email.
+
+    Args:
+        cursor (sqlite3.Cursor): Cursor in database file.
+
+    """
+    email = input("Email to lookup: ")
+    sql = 'select * from people where email like "%{}%"'
+    results = cursor.execute(sql.format(email))
+    people = results.fetchall()
+
+    for person in people:
+        print(person)
+
+
+def notes_lookup(cursor):
+    """Searches the database for user's input; sample of notes to lookup.
+
+    Note: The function will also work with a completed note.
+
+    Args:
+        cursor (sqlite3.Cursor): Cursor in database file.
+
+    """
+    notes = input("Sample notes to lookup: ")
+    sql = 'select * from people where notes like "%{}%"'
+    results = cursor.execute(sql.format(notes))
+    people = results.fetchall()
+
+    for person in people:
+        print(person)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import db_functions
 
 
 def main():
+    """Switch for Rolodex; creates connection and cursor for database."""
     conn = sqlite3.connect("rolodex.db")
     cursor = conn.cursor()
     db_functions.create_db(cursor)
@@ -13,17 +14,29 @@ def main():
 
         if choice == "add person":
             db_functions.add_person(cursor)
-        elif choice == "list people":
-            db_functions.list_people(cursor)
         elif choice == "delete person":
             db_functions.delete_person(cursor)
+        elif choice == "update person":
+            db_functions.update_person(cursor)
+        elif choice == "list people":
+            db_functions.list_people(cursor)
+        elif choice == "name lookup":
+            db_functions.list_person(cursor)
+        elif choice == "letter lookup":
+            db_functions.name_lookup(cursor)
+        elif choice == "number lookup":
+            db_functions.number_lookup(cursor)
+        elif choice == "email lookup":
+            db_functions.email_lookup(cursor)
+        elif choice == "notes lookup":
+            db_functions.notes_lookup(cursor)
         elif choice == "quit" or choice == "q":
             print("Goodbye!")
             break
         else:
             print("ERROR: input not valid.")
 
-        conn.commit()
+        conn.commit()  # Commits info every time it loops.
 
     conn.close()
 
